e6d1f3bd608fee59ee57a58016537f24dd34e31c
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2023-07-06 14:42:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function BukaPostQuran() {
+				document.getElementById ("quran-dialog").style.display ="block";
+				document.getElementById ("TutupPostQuran").style.display ="inline";
+				document.getElementById ("BukaPostQuran").style.display ="none";
+				}
+				
+function TutupPostQuran() {
+			  document.getElementById ("quran-dialog").style.display ="none";
+			  document.getElementById ("TutupPostQuran").style.display ="none";
+			  document.getElementById ("BukaPostQuran").style.display ="inline";
+				}
